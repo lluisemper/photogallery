6a01df9f2d668f046651abfce9b68cd98df5ca8d
lluisemper <lluisemper@gmail.com>
2020-04-13 12:17:46 +0200

    j

--- angular-photo-gallery/src/app/components/photo-list/photo-list.component.ts
+++ angular-photo-gallery/src/app/components/photo-list/photo-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
+import { PhotoService } from '../../services/photo.service'
+import { Photo } from '../../interfaces/Photo'
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'app-photos-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
 export class PhotoListComponent implements OnInit {
 
-  constructor() { }
+  photos: Photo[] = [];
 
-  ngOnInit(): void {
+  constructor(
+    private photoService: PhotoService,
+    private router: Router 
+  ) { }
+
+  ngOnInit() {
+    this.photoService.getPhotos()
+      .subscribe(
+        res => {
+          this.photos = res;
+        },
+        err => console.log(err)
+      )
+  }
+
+  selectedCard(id: string) {
+    this.router.navigate(['/photos', id]);
   }
 
 }
