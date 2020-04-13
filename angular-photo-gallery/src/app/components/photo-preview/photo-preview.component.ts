f248b35338f1a2e7c7f7fd807dc024ca62eb14a7
lluisemper <lluisemper@gmail.com>
2020-04-13 12:38:36 +0200

    J

--- angular-photo-gallery/src/app/components/photo-preview/photo-preview.component.ts
+++ angular-photo-gallery/src/app/components/photo-preview/photo-preview.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router'
+
+import { PhotoService } from '../../services/photo.service'
+import {Photo} from '../../interfaces/Photo'
 
 @Component({
   selector: 'app-photo-preview',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoPreviewComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  photo: Photo;
 
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.id = params['id'];
+      this.photoService.getPhoto(this.id)
+        .subscribe(
+          res => {
+            this.photo = res;
+          },
+          err => console.log(err)
+        )
+    });
+  }
+
+  deletePhoto(id: string) {
+    this.photoService.deletePhoto(id)
+      .subscribe(res => {
+        console.log(res)
+        this.router.navigate(['/photos']);
+      })
+  }
+
+  updatePhoto(title: HTMLInputElement, description: HTMLInputElement): boolean {
+    this.photoService.updatePhoto(this.photo._id, title.value, description.value)
+      .subscribe(res => {
+        console.log(res);
+        this.router.navigate(['/photos']);
+      });
+    return false;
   }
 
 }
