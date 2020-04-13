1380a15856390f4d67a06946461982e1ef641708
lluisemper <lluisemper@gmail.com>
2020-04-13 12:09:32 +0200

    j

--- photogalery/src/app.ts
+++ photogalery/src/app.ts
@@ -1,14 +1,17 @@
-import express from 'express';
+import express, {Application} from 'express';
 import morgan from 'morgan';
-import path from 'path'
+import path from 'path';
+import cors from 'cors';
 
-const app = express();
+
+const app: Application = express();
 
 import indexRoutes from './routes/index'
 
 app.set('port', process.env.PORT || 4000);
 
 app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', indexRoutes);
